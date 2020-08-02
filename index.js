1244f746fed748930c176a1f5a8b33869b2b71b2
amiranjom <ar.anjomshoaa@gmail.com>
2020-08-02 10:18:00 -0700

    first mutation

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-import express from 'express';
-import {graphqlHTTP} from 'express-graphql';
-import schema from './schema';
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res) => {
-    res.send("Hi")
+app.get("/", (req, res) => {
+  res.send("Hi");
 });
 
-const root = { friend: () => {
-    return {
-        "id": 19999,
-        "firstName": "Amir",
-        "lastName": "Anjomshoaa",
-        "gender": "Man",
-        "language": "Farsi",
-        "email": "me@me.com"
+class Friend {
+    constructor(id, {firstName, lastName, gender, language, email}){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.language = language;
+        this.email = email;
     }
-}};
+}
 
-app.use('/graphql', graphqlHTTP({
+const friendDatabase = {};
+
+const root = {
+  friend: () => {
+    return {
+      id: 19999,
+      firstName: "Amir",
+      lastName: "Anjomshoaa",
+      gender: "Man",
+      language: "Farsi",
+      emails: [{ email: "me@me.com" }, { email: "test@test.com" }],
+    };
+  },
+  createFriend: ({input}) => {
+      let id = require('crypto').randomBytes(10).toString('hex');
+      friendDatabase[id] = input
+      return new Friend(id,input)
+  }
+};
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
-    graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
-
-app.listen(8080,()=>{console.log("Running on 8080")});
+app.listen(8080, () => {
+  console.log("Running on 8080");
+});
